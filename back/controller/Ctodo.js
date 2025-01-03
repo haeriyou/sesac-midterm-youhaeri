d0eea4095953d3b4ec6d20877d385b2b5849a4eb
haeriyou <haeri1you@gmail.com>
2025-01-03 15:13:46 +0900

    backend 중간 커밋

---
+++ back/controller/Ctodo.js
@@ -0,0 +1,68 @@
+const { Todo } = require("../models/index");
+
+/* Todos 전체 목록 불러오기 */
+exports.readAll = async (req, res) => {
+  try {
+    const todos = await Todo.findAll();
+    res.send(todos);
+  } catch (err) {
+    console.log("Todo not found", err);
+    res.status(500).send("internal server error");
+  }
+};
+
+/* Todo 한 개 불러오기 */
+exports.readOne = async (req, res) => {
+  try {
+    const { todoId } = req.params;
+    const todos = await Todo.fineOne({
+      where: { id: todoId },
+    });
+    res.send(todos);
+  } catch (err) {
+    console.log("internal server error", err);
+    res.status(500).send("Todo not found");
+  }
+};
+
+/* 새로운 Todo 생성 */
+exports.create = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log("internal server error", err);
+    res.status(500).send("Todo not found");
+  }
+};
+
+/* 기존 Todo 수정 */
+exports.update = async (req, res) => {
+  try {
+    const updateTodo = await Todo.update(
+      { title: req.body.title },
+      { where: { id: req.body.id } }
+    );
+    res.send(updateTodo);
+  } catch (err) {
+    console.log("internal server error", err);
+    res.status(500).send("Todo not found");
+  }
+};
+
+/* 기존 Todo 삭제 */
+exports.delete = async (req, res) => {
+  try {
+    const { todoId } = req.params;
+    const isDeleted = await Todo.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if (Boolean(isDeleted)) {
+      res.send("Todo deleted successfully");
+    } else res.send(false);
+    res.send("deleted");
+  } catch (err) {
+    console.log("internal server error", err);
+    res.status(500).send("Todo not found");
+  }
+};
